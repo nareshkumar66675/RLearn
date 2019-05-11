820fe9e2f3530aa5aefbb5891a3062353d3fddf3
Naresh Kumar <nareshkumar66675@gmail.com>
2019-05-10 23:05:24 -0500

    RLearnProject

---
+++ Map.py
@@ -0,0 +1,103 @@
+import sys
+import numpy as np
+from Utils import *
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+
+DefaultMap = [
+        "S-------",
+        "--------",
+        "---x----",
+        "-----x--",
+        "---x----",
+        "-xx---x-",
+        "-x--x-x-",
+        "---x---T"
+    ]
+
+
+class Map():
+
+
+    def __init__(self, generate=True, customMap = None):
+
+        if generate == True and customMap == None:
+            desc = generate_random_map()
+        elif customMap != None:
+            desc = customMap
+        else:
+            desc=DefaultMap
+
+        self.desc = desc = np.asarray(desc,dtype='c')
+        self.nrow, self.ncol = nrow, ncol = desc.shape
+        self.reward_range = (0, 1)
+
+        self.nActions = 4
+        self.nStates = nrow * ncol
+
+        self.isd = np.array(desc == b'S').astype('float64').ravel()
+        self.isd /= self.isd.sum()
+
+        self.P = {s : {a : [] for a in range(self.nActions)} for s in range(self.nStates)}
+
+        def to_s(row, col):
+            return row*ncol + col
+
+        def increment(row, col, a):
+            if a == LEFT:
+                col = max(col-1,0)
+            elif a == DOWN:
+                row = min(row+1,nrow-1)
+            elif a == RIGHT:
+                col = min(col+1,ncol-1)
+            elif a == UP:
+                row = max(row-1,0)
+            return (row, col)
+
+        for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for a in range(4):
+                    li = self.P[s][a]
+                    letter = desc[row, col]
+                    if letter in b'Tx':
+                        li.append((1.0, s, 0, True))
+                    else:
+                        newrow, newcol = increment(row, col, a)
+                        newstate = to_s(newrow, newcol)
+                        newletter = desc[newrow, newcol]
+                        done = bytes(newletter) in b'Tx'
+                        rew = float(newletter == b'T')
+                        li.append((1.0, newstate, rew, done))
+
+
+
+    def printMap(self):
+        row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
+        desc = [[c.decode('utf-8') for c in line] for line in desc]
+        desc[row][col] = "(M)".format(desc[row][col])
+        if self.lastaction is not None:
+            print("  ({})\n".format(["Left","Down","Right","Up"][self.lastaction]))
+        else:
+            print("\n")
+        print("\n".join('  '.join(line) for line in desc)+"\n")
+
+
+    
+    def clear(self):
+        self.s = categorical_sample(self.isd, np.random)
+        self.lastaction = None
+        return self.s
+
+    def PerformAction(self, a):
+        transitions = self.P[self.s][a]
+        i = categorical_sample([t[0] for t in transitions], np.random)
+        p, s, r, d= transitions[i]
+        self.s = s
+        self.lastaction = a
+        return (s, r, d, {"prob" : p})
--- RLearn.py
+++ RLearn.py
@@ -1,1 +1,88 @@
 
+import numpy as np 
+import Map as mp
+
+
+def RunAlgo(customData = None):
+    map = mp.Map(customMap=customData)
+    QTable = np.zeros([map.nStates,map.nActions])
+    # QLearn Parameters
+    eta = .628
+    gma = .9
+    epis = 5000
+    rev_list = [] 
+    for i in range(epis):
+        state = map.clear()
+        rAll = 0
+        decision = False
+        j = 0
+       # Set Qtable
+        while j < 99:
+            j+=1
+            # Choose action from Q table
+            action = np.argmax(QTable[state,:] + np.random.randn(1,map.nActions)*(1./(i+1)))
+            # Get New State and Reward Vlaues
+            newState,reward,decision,_ = map.PerformAction(action)
+            QTable[state,action] = QTable[state,action] + eta*(reward + gma*np.max(QTable[newState,:]) - QTable[state,action])
+            rAll += reward
+            state = newState
+            if decision == True:
+                break
+        rev_list.append(rAll)
+    print("Sum of Rewards:", str(sum(rev_list)/epis))
+
+
+    # Test the algorithm
+    state = map.clear()
+    decision = False
+    reward=0.0
+    while decision != True:
+        map.printMap()
+        action = np.argmax(QTable[state,:] + np.random.randn(1,map.nActions)*(1./(i+1)))
+        newState,reward,decision,_ = map.PerformAction(action)
+        QTable[state,action] = QTable[state,action] + eta*(reward + gma*np.max(QTable[newState,:]) - QTable[state,action])
+        state = newState
+    map.printMap()
+    if reward==0.0:
+        print("Failed Attempt")
+    else:
+        print("Success Attempt")
+
+def GetCustomMap():
+    print("Enter Custom Map")
+    print("Sample Map:")
+
+    print("\n".join(mp.DefaultMap))
+
+    cMap = []
+
+    for i in range(1,9):
+        line = input('Enter Line {0}:'.format(i))
+        cMap.append(line)
+
+    return cMap
+
+print(" Reinforcement Learning using QLearn")
+print(" Map Legend ")
+print("S: Start \nx: Obstacle \n-:Free Path \nT: Treasure \n(M): Current Position \n")
+
+print("-----------------------------------------------\n\n")
+
+while True:
+    print("Select Maps")
+    print("1. Auto Generate Map")
+    print("2. Enter Custom Map")
+
+    userChoice = int(input('Select one option from above : '))
+
+    if userChoice == 1:
+        RunAlgo(None)
+    elif userChoice == 2:
+        cMap = GetCustomMap()
+        RunAlgo(cMap)
+    else:
+        choice = input('Enter Valid Option. Press Y to Restart and N to Exit: ')
+        if str.lower(choice) == 'n':
+            sys.exit()
+        else:
+            continue
